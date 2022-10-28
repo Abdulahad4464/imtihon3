e0202c56b0e16832cfcc70a0bcfe75cd286e6ed0
Abdulahad4464 <abdulahadabdurahmanov25@gmail.com>
2022-10-28 14:33:03 +0500

    imtihon3

---
+++ app.js
@@ -0,0 +1,16 @@
+const home = document.querySelector(".home")
+const about = document.querySelector(".about")
+const abous = document.querySelector(".abous")
+
+
+function showHome(){
+    home.classList.toggle("show_home")
+}
+
+function showabout(){
+    about.classList.toggle("show_about")
+}
+
+function showabous(){
+    abous.classList.toggle("show_abous")
+}
